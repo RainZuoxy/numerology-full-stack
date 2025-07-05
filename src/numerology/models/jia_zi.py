003465ed8a033e8916732e13cfd2a010d0ca1e8c
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-07-06 01:31:58 +0800

    fix: Fixed the time range mapped to ZI to support both 23:00-23:59 and 00:00-01:00.(left closed and right open)

--- src/numerology/models/jia_zi.py
+++ src/numerology/models/jia_zi.py
@@ -2,7 +2,7 @@ from typing import List
 from datetime import time
 from numerology.const.wu_xing import TianGanType, DiZhiType, JIA_ZI
 from numerology.models.pillar import PillarElement, PillarEarthElement
-from numerology.models.wu_xing import WuXingSet
+from numerology.models.wu_xing import WuXingRegister
 
 
 class TianGan:
@@ -12,16 +12,16 @@ class TianGan:
     @classmethod
     def create_tian_gan_list(cls) -> List[PillarElement]:
         return [
-            PillarElement(name=TianGanType.JIA.value, positive=True, sequence=1, wu_xing=WuXingSet.get_wood()),
-            PillarElement(name=TianGanType.YI.value, positive=False, sequence=2, wu_xing=WuXingSet.get_wood()),
-            PillarElement(name=TianGanType.BING.value, positive=True, sequence=3, wu_xing=WuXingSet.get_fire()),
-            PillarElement(name=TianGanType.DING.value, positive=False, sequence=4, wu_xing=WuXingSet.get_fire()),
-            PillarElement(name=TianGanType.WU.value, positive=True, sequence=5, wu_xing=WuXingSet.get_earth()),
-            PillarElement(name=TianGanType.JI.value, positive=False, sequence=6, wu_xing=WuXingSet.get_earth()),
-            PillarElement(name=TianGanType.GENG.value, positive=True, sequence=7, wu_xing=WuXingSet.get_metal()),
-            PillarElement(name=TianGanType.XIN.value, positive=False, sequence=8, wu_xing=WuXingSet.get_metal()),
-            PillarElement(name=TianGanType.REN.value, positive=True, sequence=9, wu_xing=WuXingSet.get_water()),
-            PillarElement(name=TianGanType.GUI.value, positive=False, sequence=10, wu_xing=WuXingSet.get_water())
+            PillarElement(name=TianGanType.JIA.value, positive=True, sequence=1, wu_xing=WuXingRegister.get_wood()),
+            PillarElement(name=TianGanType.YI.value, positive=False, sequence=2, wu_xing=WuXingRegister.get_wood()),
+            PillarElement(name=TianGanType.BING.value, positive=True, sequence=3, wu_xing=WuXingRegister.get_fire()),
+            PillarElement(name=TianGanType.DING.value, positive=False, sequence=4, wu_xing=WuXingRegister.get_fire()),
+            PillarElement(name=TianGanType.WU.value, positive=True, sequence=5, wu_xing=WuXingRegister.get_earth()),
+            PillarElement(name=TianGanType.JI.value, positive=False, sequence=6, wu_xing=WuXingRegister.get_earth()),
+            PillarElement(name=TianGanType.GENG.value, positive=True, sequence=7, wu_xing=WuXingRegister.get_metal()),
+            PillarElement(name=TianGanType.XIN.value, positive=False, sequence=8, wu_xing=WuXingRegister.get_metal()),
+            PillarElement(name=TianGanType.REN.value, positive=True, sequence=9, wu_xing=WuXingRegister.get_water()),
+            PillarElement(name=TianGanType.GUI.value, positive=False, sequence=10, wu_xing=WuXingRegister.get_water())
         ]
 
     @classmethod
@@ -56,52 +56,56 @@ class DiZhi:
     def create_di_zhi_list(cls) -> List[PillarEarthElement]:
         return [
             PillarEarthElement(
+                name=DiZhiType.ZI.value, positive=True, sequence=0,
+                start_time=time(23, 0, 0), end_time=time(23, 59, 59), wu_xing=WuXingRegister.get_water()
+            ),
+            PillarEarthElement(
                 name=DiZhiType.ZI.value, positive=True, sequence=1,
-                start_time=time(23, 0, 0), end_time=time(1, 0, 0), wu_xing=WuXingSet.get_water()
+                start_time=time(0, 0, 0), end_time=time(0, 59, 59), wu_xing=WuXingRegister.get_water()
             ),
             PillarEarthElement(
                 name=DiZhiType.CHOU.value, positive=False, sequence=2,
-                start_time=time(1, 0, 0), end_time=time(3, 0, 0), wu_xing=WuXingSet.get_earth()
+                start_time=time(1, 0, 0), end_time=time(2, 59, 59), wu_xing=WuXingRegister.get_earth()
             ),
             PillarEarthElement(
                 name=DiZhiType.YIN.value, positive=True, sequence=3,
-                start_time=time(3, 0, 0), end_time=time(5, 0, 0), wu_xing=WuXingSet.get_wood()
+                start_time=time(3, 0, 0), end_time=time(4, 59, 59), wu_xing=WuXingRegister.get_wood()
             ),
             PillarEarthElement(
                 name=DiZhiType.MAO.value, positive=False, sequence=4,
-                start_time=time(5, 0, 0), end_time=time(7, 0, 0), wu_xing=WuXingSet.get_wood()
+                start_time=time(5, 0, 0), end_time=time(6, 59, 59), wu_xing=WuXingRegister.get_wood()
             ),
             PillarEarthElement(
                 name=DiZhiType.CHEN.value, positive=True, sequence=5,
-                start_time=time(7, 0, 0), end_time=time(9, 0, 0), wu_xing=WuXingSet.get_earth()
+                start_time=time(7, 0, 0), end_time=time(8, 59, 59), wu_xing=WuXingRegister.get_earth()
             ),
             PillarEarthElement(
                 name=DiZhiType.SI.value, positive=False, sequence=6,
-                start_time=time(9, 0, 0), end_time=time(11, 0, 0), wu_xing=WuXingSet.get_fire()
+                start_time=time(9, 0, 0), end_time=time(10, 59, 59), wu_xing=WuXingRegister.get_fire()
             ),
             PillarEarthElement(
                 name=DiZhiType.WU.value, positive=True, sequence=7,
-                start_time=time(11, 0, 0), end_time=time(13, 0, 0), wu_xing=WuXingSet.get_fire()
+                start_time=time(11, 0, 0), end_time=time(12, 59, 59), wu_xing=WuXingRegister.get_fire()
             ),
             PillarEarthElement(
                 name=DiZhiType.WEI.value, positive=False, sequence=8,
-                start_time=time(13, 0, 0), end_time=time(15, 0, 0), wu_xing=WuXingSet.get_earth()
+                start_time=time(13, 0, 0), end_time=time(14, 59, 59), wu_xing=WuXingRegister.get_earth()
             ),
             PillarEarthElement(
                 name=DiZhiType.SHEN.value, positive=True, sequence=9,
-                start_time=time(15, 0, 0), end_time=time(17, 0, 0), wu_xing=WuXingSet.get_metal()
+                start_time=time(15, 0, 0), end_time=time(16, 59, 59), wu_xing=WuXingRegister.get_metal()
             ),
             PillarEarthElement(
                 name=DiZhiType.YOU.value, positive=False, sequence=10,
-                start_time=time(17, 0, 0), end_time=time(19, 0, 0), wu_xing=WuXingSet.get_metal()
+                start_time=time(17, 0, 0), end_time=time(18, 59, 59), wu_xing=WuXingRegister.get_metal()
             ),
             PillarEarthElement(
                 name=DiZhiType.XU.value, positive=True, sequence=11,
-                start_time=time(19, 0, 0), end_time=time(21, 0, 0), wu_xing=WuXingSet.get_earth()
+                start_time=time(19, 0, 0), end_time=time(20, 59, 59), wu_xing=WuXingRegister.get_earth()
             ),
             PillarEarthElement(
                 name=DiZhiType.HAI.value, positive=False, sequence=12,
-                start_time=time(21, 0, 0), end_time=time(23, 0, 0), wu_xing=WuXingSet.get_water()
+                start_time=time(21, 0, 0), end_time=time(22, 59, 59), wu_xing=WuXingRegister.get_water()
             )
         ]
 
@@ -130,12 +134,12 @@ class DiZhi:
 
     @classmethod
     def get_di_zhi_by_hour(cls, t: time) -> PillarEarthElement:
-        di_zhi = [item for item in cls.get_di_zhi_list() if item.start_time <= t < item.end_time]
 
-        if len(di_zhi) == 0:
-            raise ValueError(f'无法查找{time}时辰的地支')
+        for item in cls.get_di_zhi_list():
+            if item.start_time <= t < item.end_time:
+                return item
 
-        return di_zhi[0]
+        raise ValueError(f'无法查找{time}时辰的地支')
 
 
 class JiaZi:
