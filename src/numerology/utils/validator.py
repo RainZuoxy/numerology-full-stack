1d6d9e73a1903a530f9a377795f900b5f2eee988
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-07-12 01:24:28 +0800

    feature: Add method to validate integer range

---
+++ src/numerology/utils/validator.py
@@ -0,0 +1,28 @@
+from typing import Callable, Union
+
+
+def validate_int_range(
+        *,
+        min_value: int, max_value: int,
+        param: Union[str, None] = None,
+        # class_:bool = False,
+        error_msg: Union[str, None] = None
+):
+    def decorator(func: Callable):
+        def wrapper(*args, **kwargs):
+            validate_param = kwargs.get(param) or args[1]
+            if param and not validate_param:
+                raise ValueError(f"Can not find parameter name:'{param}'")
+            if not (isinstance(min_value, int) and isinstance(max_value, int)):
+                raise TypeError("Parameters: min_value, max_value are not integer")
+            if not (min_value <= validate_param <= max_value):
+                err_msg = f"parameter::index is out of validation({min_value}~{max_value})"
+                if error_msg and isinstance(error_msg, str):
+                    err_msg = f"{error_msg}\n{err_msg}"
+                raise IndexError(err_msg)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
