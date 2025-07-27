acd0809d0b3f2d713f3deb179528e2fc843f81ad
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-07-27 23:30:11 +0800

    feature: Remove useless modules

--- src/numerology/models/ba_zi.py
+++ src/numerology/models/ba_zi.py
@@ -1,27 +1,23 @@
 from datetime import datetime
+from typing import Any
+
 from pydantic import BaseModel
-# from numerology.models.wu_xing import WuXingRegister
+
+from numerology.const.tian_gan import TianGanType
+from numerology.core.rule import get_pillar_year_index, get_pillar_month_index, get_pillar_day_index
+from numerology.models.wu_xing import WuXingRegister
+from numerology.models import TIAN_GAN, DI_ZHI
 from numerology.models.pillar import PillarItem
-from numerology.models.jia_zi import TianGan, DiZhi, JiaZi
+from numerology.models.jia_zi import JiaZi
 
 
-class BaZiGenerator(BaseModel):
-    dob_time: datetime
+class BaZiChart(BaseModel):
     lunar_dob_time: datetime
+    day_master: TianGanType = None
     year_pillar: PillarItem = None
     month_pillar: PillarItem = None
     day_pillar: PillarItem = None
     hour_pillar: PillarItem = None
-
-    # def model_post_init(self,__context: Any):
-    #     self.init()
-    #
-    # @staticmethod
-    # def init():
-    #     WuXingSet.init()
-
-    def get_dob_time(self):
-        return self.dob_time
 
     def get_lunar_dob_time(self):
         return self.lunar_dob_time
@@ -34,20 +30,22 @@ class BaZiGenerator(BaseModel):
         self.month_pillar = self.compute_month_pillar(
             year=self.get_lunar_dob_time().year, month=self.get_lunar_dob_time().month
         )
-        self.day_pillar = self.compute_day_pillar(dt=self.get_dob_time())
+        self.day_pillar = self.compute_day_pillar(solar_dt=self.get_dob_time())
         self.hour_pillar = self.compute_hour_pillar(dt=self.get_dob_time())
+        self.day_master = self.day_pillar.tian_gan.name
         self.analysis_shi_shen()
 
     '''
         年份干支口诀:
-        天干: (公元年份-3)/10取余
-        地支: (公元年份-3)/12取余
+        天干: (阴历年份-3)/10取余
+        地支: (阴历年份-3)/12取余
     '''
+
     @classmethod
     def compute_year_pillar(cls, year: int) -> PillarItem:
+        tiangan_index, dizhi_index = get_pillar_year_index(dob_year=year)
         return PillarItem(
-            name='年柱', tian_gan=TianGan.get_tian_gan_by_index(int((year - 3) % 10)),
-            di_zhi=DiZhi.get_di_zhi_by_index(int((year - 3) % 12))
+            name='年柱', tian_gan=TIAN_GAN[tiangan_index], di_zhi=DI_ZHI[dizhi_index]
         )
 
     '''
@@ -59,19 +57,21 @@ class BaZiGenerator(BaseModel):
     凡丁年壬年, 一月天干为壬, 二月天干为癸, 其余顺推;
     凡戊年癸年, 一月天干为甲, 二月天干为乙, 其余顺推;
     地支:
-    每月的地支是固定的, 正月为寅, 二月为卯, 三月为辰, 四月为巳, 五月为午, 六月为未, 
+    每月的地支是固定的, 正月为寅, 二月为卯, 三月为辰, 四月为巳, 五月为午, 六月为未,
     七月为申, 八月为酉, 九月为戌, 十月为亥, 十一月为子, 十二月为丑.
     '''
+
     @classmethod
     def compute_month_pillar(cls, year: int, month: int) -> PillarItem:
         start_pillars = {1: 3, 2: 5, 3: 7, 4: 9, 0: 9}
         year_pillar = cls.compute_year_pillar(year=year)
+        tiangan_index,dizhi_index=get_pillar_month_index(
+            sequence_year_tiangan=year_pillar.tian_gan.sequence, month=month
+        )
         return PillarItem(
             name='月柱',
-            tian_gan=TianGan.get_tian_gan_by_index(
-                month - (10 - start_pillars.get(year_pillar.tian_gan.sequence % 5) + 1)
-            ),
-            di_zhi=DiZhi.get_di_zhi_by_index(month - 10)
+            tian_gan=TIAN_GAN[tiangan_index],
+            di_zhi=DI_ZHI[dizhi_index]
         )
 
     '''
@@ -80,15 +80,14 @@ class BaZiGenerator(BaseModel):
     天干: 余数 % 10取余
     地支: 余数 % 12取余
     '''
+
     @classmethod
-    def compute_day_pillar(cls, dt: datetime) -> PillarItem:
-        day_reminder = (dt.year - 1) * 5 + (dt.year - 1) / 4
-        day_reminder += dt.date().timetuple().tm_yday
-        day_reminder = day_reminder % 60
+    def compute_day_pillar(cls, solar_dt: datetime) -> PillarItem:
+        tiangan_index,dizhi_index=get_pillar_day_index(dt=solar_dt)
 
         return PillarItem(
-            name='日柱', tian_gan=TianGan.get_tian_gan_by_index(int(day_reminder % 10)),
-            di_zhi=DiZhi.get_di_zhi_by_index(int(day_reminder % 12))
+            name='日柱', tian_gan=TIAN_GAN[tiangan_index],
+            di_zhi=DI_ZHI[dizhi_index]
         )
 
     '''
@@ -99,7 +98,7 @@ class BaZiGenerator(BaseModel):
     丙日或辛日, 子时则配戊, 即戊子, 丑时则顺排, 配己, 即己丑, 其余顺推;
     丁日或壬日, 子时则配庚, 即庚子, 其余顺排.
     戊日或癸日, 子时则配壬, 即壬子, 其余顺排.
-    
+
     地支：
     子时: 23点--凌晨1点 - 丑时: 1点--凌晨3点
     寅时: 3点--凌晨5点 - 卯时: 5点--早晨7点
@@ -108,9 +107,10 @@ class BaZiGenerator(BaseModel):
     申时: 15点--下午17点 - 酉时: 17点--下午19点
     戌时: 19点--晚上21点 - 亥时: 21点--晚上23点
     '''
+
     @classmethod
     def compute_hour_pillar(cls, dt: datetime) -> PillarItem:
-        day_pillar = cls.compute_day_pillar(dt=dt)
+        day_pillar = cls.compute_day_pillar(solar_dt=dt)
         tian_gan_index = day_pillar.tian_gan.sequence % 5
         di_zhi = DiZhi.get_di_zhi_by_hour(dt.time())
         jia_zi = JiaZi.get_jia_zi_by_index(tian_gan_index=tian_gan_index, di_zhi_index=di_zhi.sequence)
@@ -126,3 +126,7 @@ class BaZiGenerator(BaseModel):
         self.day_pillar.di_zhi.get_shi_shen(day_master=self.day_pillar.tian_gan)
         self.hour_pillar.tian_gan.get_shi_shen(day_master=self.day_pillar.tian_gan)
         self.hour_pillar.di_zhi.get_shi_shen(day_master=self.day_pillar.tian_gan)
+
+
+if __name__ == '__main__':
+    print(2022 % 12)
