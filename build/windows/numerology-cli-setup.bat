b86763c4c23055ed83994430c27cab63469d9603
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-08-08 20:29:58 +0800

    fix: Fix an issue in windows setup script

--- build/windows/numerology-cli-setup.bat
+++ build/windows/numerology-cli-setup.bat
@@ -4,45 +4,53 @@ cls
 
 set PROJECT_NAME=numerology-cli
 set VERSION=0.1.0
-set ENTRYPOINT_FILE=./src/numerology_cli/entrypoint/__init__.py
+set ENTRYPOINT_FILE_NAME=__init__
+set ENTRYPOINT_FILE=.\src\numerology_cli\entrypoint\%ENTRYPOINT_FILE_NAME%.py
 set PACKAGE_NAME=%PROJECT_NAME%-%VERSION%.tar
-set NUITKA_OUTPUT_DIR=./build
-set NUITKA_OPTIONS=--standalone --assume-yes-for-downloads --output-filename=%PROJECT_NAME% --output-dir=%NUITKA_OUTPUT_DIR% %ENTRYPOINT_FILE%
+set NUITKA_OUTPUT_DIR=.\build
+set NUITKA_OPTIONS=--standalone --assume-yes-for-downloads --nofollow-import-to=pydantic.v1.* --output-filename=%PROJECT_NAME% --output-dir=%NUITKA_OUTPUT_DIR% %ENTRYPOINT_FILE%
 
 
 echo Prepare for environment
 uv sync --active --no-cache
-echo %ERRORLEVEL%
 if %ERRORLEVEL% NEQ 0 (
+    set UV_DEFAULT_INDEX=https://mirrors.aliyun.com/pypi/simple/
+    uv sync --active --no-cache
     echo Failed to compile and build
     exit /B %ERRORLEVEL%
+)
+
+echo Check output dir
+if exist %NUITKA_OUTPUT_DIR% (
+    rd /S /Q %NUITKA_OUTPUT_DIR%
+    echo Clean origin output dir
 )
 
 echo ====================Building====================
 echo Step1: compile and build
 uv run -- nuitka %NUITKA_OPTIONS%
-echo %ERRORLEVEL%
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to compile and build
     exit /B %ERRORLEVEL%
 )
 
 echo Step2: rename folder
-ren build\__init__.dist %PROJECT_NAME%
+ren %NUITKA_OUTPUT_DIR%\%ENTRYPOINT_FILE_NAME%.dist %PROJECT_NAME%
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to rename folder
     exit /B %ERRORLEVEL%
 )
 
 echo Step3: verify
-%PROJECT_NAME%/n%PROJECT_NAME%.exe --help
+%NUITKA_OUTPUT_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.exe --help
+%NUITKA_OUTPUT_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.exe -v
 if %ERRORLEVEL% NEQ 0 (
     echo %PROJECT_NAME% is not working 1>&2
     exit /B %ERRORLEVEL%
 )
 
 echo Step4: package
-tar -cvf %PACKAGE_NAME% %PROJECT_NAME%
+tar -cvf %PACKAGE_NAME% %NUITKA_OUTPUT_DIR%\%PROJECT_NAME%
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to package 1>&2
     exit /B %ERRORLEVEL%
