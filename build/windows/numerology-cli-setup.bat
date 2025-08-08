37dedc3d6f88656590ca1a85545d52a26d4aae56
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-08-08 16:23:28 +0800

    refactor: Optimize setup script

--- build/windows/numerology-cli-setup.bat
+++ build/windows/numerology-cli-setup.bat
@@ -10,35 +10,42 @@ set NUITKA_OUTPUT_DIR=./build
 set NUITKA_OPTIONS=--standalone --assume-yes-for-downloads --output-filename=%PROJECT_NAME% --output-dir=%NUITKA_OUTPUT_DIR% %ENTRYPOINT_FILE%
 
 
-echo ====================Building====================
+echo Prepare for environment
+uv sync --active --no-cache
 echo %ERRORLEVEL%
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to compile and build
+    exit /B %ERRORLEVEL%
+)
+
+echo ====================Building====================
 echo Step1: compile and build
 uv run -- nuitka %NUITKA_OPTIONS%
 echo %ERRORLEVEL%
-if %ERRORLEVEL% NEQ 0 {
+if %ERRORLEVEL% NEQ 0 (
     echo Failed to compile and build
     exit /B %ERRORLEVEL%
-}
+)
 
 echo Step2: rename folder
 ren build\__init__.dist %PROJECT_NAME%
-if %ERRORLEVEL% NEQ 0 {
+if %ERRORLEVEL% NEQ 0 (
     echo Failed to rename folder
     exit /B %ERRORLEVEL%
-}
+)
 
 echo Step3: verify
 %PROJECT_NAME%/n%PROJECT_NAME%.exe --help
-if %ERRORLEVEL% NEQ 0 {
+if %ERRORLEVEL% NEQ 0 (
     echo %PROJECT_NAME% is not working 1>&2
     exit /B %ERRORLEVEL%
-}
+)
 
 echo Step4: package
 tar -cvf %PACKAGE_NAME% %PROJECT_NAME%
-if %ERRORLEVEL% NEQ 0 {
+if %ERRORLEVEL% NEQ 0 (
     echo Failed to package 1>&2
     exit /B %ERRORLEVEL%
-}
+)
 
 echo ===========Building successfully done===========
