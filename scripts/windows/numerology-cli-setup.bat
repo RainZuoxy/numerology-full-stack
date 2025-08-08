5bc10714fc2849a964af03dedca9ae0b5485a56b
Michael_Zuo <michael_zuoxy@foxmail.com>
2025-08-09 02:27:47 +0800

    refactor: Optimize Github workflow and win-setup script

--- scripts/windows/numerology-cli-setup.bat
+++ scripts/windows/numerology-cli-setup.bat
@@ -11,39 +11,32 @@ set NUITKA_OUTPUT_DIR=dist
 set NUITKA_OPTIONS=--standalone --assume-yes-for-downloads --nofollow-import-to=pydantic.v1.* --output-filename=%PROJECT_NAME% --output-dir=%NUITKA_OUTPUT_DIR% %ENTRYPOINT_FILE%
 
 
-echo Check output dir
+echo Check output dir...
 if exist %NUITKA_OUTPUT_DIR% (
     rd /S /Q %NUITKA_OUTPUT_DIR%
     echo Clean origin output dir
 )
 
 echo ====================Building====================
-echo Step1: compile and build
+echo Step1: compile and build...
 uv run -- nuitka %NUITKA_OPTIONS%
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to compile and build
     exit /B %ERRORLEVEL%
 )
 
-echo Step2: rename folder
+echo Step2: rename folder...
 ren %NUITKA_OUTPUT_DIR%\%ENTRYPOINT_FILE_NAME%.dist %PROJECT_NAME%
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to rename folder
     exit /B %ERRORLEVEL%
 )
 
-echo Step3: verify
+echo Step3: verify...
 %NUITKA_OUTPUT_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.exe --help
 %NUITKA_OUTPUT_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.exe -v
 if %ERRORLEVEL% NEQ 0 (
     echo %PROJECT_NAME% is not working 1>&2
-    exit /B %ERRORLEVEL%
-)
-
-echo Step4: package
-tar -cvf %NUITKA_OUTPUT_DIR%\%PACKAGE_NAME% %NUITKA_OUTPUT_DIR%\%PROJECT_NAME%
-if %ERRORLEVEL% NEQ 0 (
-    echo Failed to package 1>&2
     exit /B %ERRORLEVEL%
 )
 
